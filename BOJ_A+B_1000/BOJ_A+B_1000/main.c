224f49f6e11377ed54c5c4bec44df4d87ea2a8aa
Joonhee Lee <joonheelee@Joonhees-MacBook-Pro.local>
2019-01-09 02:51:47 +0900

    1000번 풀이

---
+++ BOJ_A+B_1000/BOJ_A+B_1000/main.c
@@ -0,0 +1,9 @@
+//
+//  main.c
+//  BOJ_A+B_1000
+//
+//  Created by Joonhee Lee on 09/01/2019.
+//  Copyright © 2019 Joonhee Lee. All rights reserved.
+//
+#include <stdio.h>
+int a,b=0;int main(){for(int i=0;i<2;i++){scanf("%d",&a);b=b+a;}printf("%d",b);return 0;}
